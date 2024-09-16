fa445bec355a60b6a59c4b1366f01c42973b701a
FireHunterLM <146267068+FireHunterLM@users.noreply.github.com>
2024-09-17 00:21:55 +0300

    Update Lib1.hs
    Lib1.hs failo pakeitimai

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,9 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = 
+    [ "create", "mix", "inspect", "discard", "exit"
+    , "healing", "mana", "strength", "invisibility"
+    , "unicorn_hair", "dragon_scale", "phoenix_feather", "nightshade", "mandrake_root"
+    , "and", "with"
+    ]
